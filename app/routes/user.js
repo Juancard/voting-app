aed89e2b755ccc95bc62c33e4559bc35eb608021
juan <juancruz.23@hotmail.com>
2017-02-11 12:47:40 -0300

    user data has new url

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,7 +2,7 @@
 
 module.exports = function (app, appEnv) {
 
-  app.route('/api/:user_id')
+  app.route('/api/user/:user_id([a-fA-F0-9]{24})')
 			.get(appEnv.middleware.isLoggedIn, function (req, res) {
 				res.json(req.user.twitter);
 			});
